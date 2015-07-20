45244784416e9e3c08f559085848838a3211998c
Nicole Li <nicole.li@intel.com>
2015-07-20 15:02:25 +0800

    Candy: remove 'ply-image' and use 'display_message' for Freon
    'ply-image' is no longer supported in Freon-based images.
    Switch to 'display_message' to display wiping message
    
    BUG=chrome-os-partner: 42066
    TEST=Factory wipe and battery cutoff, check the output message.
    
    Change-Id: Ic26e867af7f24aa8dafa6dbfddd9740885f02885
    Reviewed-on: https://chromium-review.googlesource.com/289432
    Reviewed-by: Bowgo Tsai <bowgotsai@chromium.org>
    Commit-Queue: Nicole Li <nicole.li@intel.com>
    Tested-by: Nicole Li <nicole.li@intel.com>

--- overlay-candy/chromeos-base/chromeos-bsp-candy/files/board_discharge_battery.sh
+++ overlay-candy/chromeos-base/chromeos-bsp-candy/files/board_discharge_battery.sh
@@ -8,9 +8,8 @@
 # it asks for AC power and waits for the battery to charge.
 #
 
-IMG_PATH="/usr/share/factory/images"
 MAX_LEVEL=60
-TTY="/dev/tty1"
+DISPLAY_MESSAGE="/usr/sbin/display_wipe_message.sh"
 
 get_percentage() {
    power_supply_info 2>/dev/null \
@@ -24,14 +23,7 @@ if [[ $(get_percentage) -gt $MAX_LEVEL ]]; then
   if (ectool battery | grep -q AC_PRESENT); then
     # If WP enabled, unplugging AC is the only way to discharge.
     if (crossystem sw_wpsw_boot?1 wpsw_boot?1); then
-      if [ -e "${IMG_PATH}/remove_ac.png" ]; then
-        ply-image --clear 0x000000 "${IMG_PATH}/remove_ac.png"
-      else
-        echo "============================================" >"$TTY"
-        echo "========== Unplug AC adapter now. ==========" >"$TTY"
-        echo "============================================" >"$TTY"
-        echo "" >"$TTY"
-      fi
+      "${DISPLAY_MESSAGE}" "remove_ac"
       while (ectool battery | grep -q AC_PRESENT) ; do
         sleep 0.5;
       done
@@ -40,19 +32,9 @@ if [[ $(get_percentage) -gt $MAX_LEVEL ]]; then
     fi
   fi
 
-  if [ -e "${IMG_PATH}/discharging.png" ]; then
-    ply-image --clear 0x000000 "${IMG_PATH}/discharging.png"
-  else
-    echo "============================================" >"$TTY"
-    echo "================ Discharging ===============" >"$TTY"
-    echo "============================================" >"$TTY"
-    echo "" >"$TTY"
-  fi
-
   # Wait for battery to discharge to MAX_LEVEL.
   while [[ $(get_percentage) -gt $MAX_LEVEL ]]; do
-    printf "\033[0;0H\033[K" >"$TTY"
-    echo -n "Current Battery Level: $(get_percentage)%" >"$TTY"
+    "${DISPLAY_MESSAGE}" "discharging" "$(get_percentage)" "${MAX_LEVEL}"
     sleep 1
   done
 
