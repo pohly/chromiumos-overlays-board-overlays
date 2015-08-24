e10f5664235c41ecd435c174e9740edf9add9a2c
Joe Konno <joe.konno@intel.com>
2015-08-24 14:13:00 -0700

    Revert "overlay-glados: add i915.preliminary_hw_support"
    This reverts commit 1d5eb76b354c8a2ae058154ed0e0c58b7c168397.
    
    Since the second i915 forklift was merged (chrome-os-partner:43344),
    these arguments are no longer necessary. Remove this work-around so that
    relevant security tests may pass.
    
    BUG=chrome-os-partner:43787
    TEST=Glados: ensure_secure_kernelparams.sh test passes
    
    Change-Id: I5036109bd5fc909b77472052ff8a3926885bc226
    Signed-off-by: Joe Konno <joe.konno@intel.com>
    Reviewed-on: https://chromium-review.googlesource.com/295258
    Commit-Ready: Joe Konno <joe.konno@linux.intel.com>
    Tested-by: Philip Hanson <philip.hanson@intel.com>
    Reviewed-by: Marc Herbert <marc.herbert@intel.com>
    Reviewed-by: James Ausmus <james.ausmus@intel.com>
    Reviewed-by: Bernie Thompson <bhthompson@chromium.org>

--- overlay-glados/scripts/build_kernel_image.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Copyright (c) 2014 The Chromium OS Authors. All rights reserved.
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
-
-modify_kernel_command_line() {
-  echo "i915.enable_execlists=1 i915.preliminary_hw_support=1" >> "$1"
-}
