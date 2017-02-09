45a44168b57d63d6acf6c754bd6ebb7ec9de2dd3
Hung-Te Lin <hungte@chromium.org>
2017-02-09 12:05:20 +0800

    chromeos-{bsp,activate-date}-*: Speed up reading VPD cache value.
    When checking if ActivateDate or als_cal_data VPD value is already set or not,
    using the optimized vpd_get_value (0.005s) is better than dump_vpd_log (0.08s).
    
    BUG=chromium:650897
    TEST=Manually set VPD values (ActivateData, als_cal_data) and run the
         scripts, seeing proper values.
    
    Change-Id: I6ae3dae9093129c98ecb63332e404da5880d39d8
    Reviewed-on: https://chromium-review.googlesource.com/440006
    Commit-Ready: Hung-Te Lin <hungte@chromium.org>
    Tested-by: Hung-Te Lin <hungte@chromium.org>
    Reviewed-by: Mike Frysinger <vapier@chromium.org>

--- overlay-link/chromeos-base/chromeos-bsp-link/files/light-sensor-set-multiplier.sh
+++ overlay-link/chromeos-base/chromeos-bsp-link/files/light-sensor-set-multiplier.sh
@@ -7,22 +7,6 @@
 VPD_CALIBRATION_NAME=als_cal_data
 
 #
-# Fetch the value for a VPD entry by name
-# Value may be cached by dump_vpd_log
-# $1: name of the VPD entry to fetch
-#
-get_vpd_entry() {
-  local output=''
-  local vpd_entry_name=$1
-  # The VPD output format is:
-  #   "als_cal_data"="1.337"
-  output=$(dump_vpd_log --full --stdout | \
-           sed -n -r -e '/^"'${vpd_entry_name}'"=/s:^[^=]*="([^"]*)".*:\1:p')
-
-  echo "${output}"
-}
-
-#
 # main
 #
 
@@ -32,7 +16,7 @@ CACHE_FILE=${CACHE_DIR}/cal.data
 if [ -e "${CACHE_FILE}" ]; then
   ALS_CAL=$(cat "${CACHE_FILE}")
 else
-  ALS_CAL=$(get_vpd_entry ${VPD_CALIBRATION_NAME})
+  ALS_CAL="$(vpd_get_value "${VPD_CALIBRATION_NAME}")"
   mkdir -p "${CACHE_DIR}"
   echo "${ALS_CAL}" > "${CACHE_FILE}"
 fi
--- overlay-peppy/chromeos-base/chromeos-bsp-peppy/files/light-sensor-set-multiplier.sh
+++ overlay-peppy/chromeos-base/chromeos-bsp-peppy/files/light-sensor-set-multiplier.sh
@@ -18,30 +18,14 @@ VPD_CALIBRATION_NAME=als_cal_data
 VPD_LENS_COLOR_NAME=light_sensor_lens_color
 
 #
-# Fetch the value for a VPD entry by name
-# Value may be cached by dump_vpd_log
-# $1: name of the VPD entry to fetch
-#
-get_vpd_entry() {
-  local output=''
-  local vpd_entry_name=$1
-  # The VPD output format is:
-  #   "als_cal_data"="1.337"
-  output=$(dump_vpd_log --full --stdout | \
-           sed -n -r -e '/^"'${vpd_entry_name}'"=/s:^[^=]*="([^"]*)".*:\1:p')
-
-  echo "${output}"
-}
-
-#
 # main
 #
 
 # Look for per-device calibration data in the VPD
-CALIBRATION_VALUE=$(get_vpd_entry "${VPD_CALIBRATION_NAME}")
+CALIBRATION_VALUE="$(vpd_get_value "${VPD_CALIBRATION_NAME}")"
 if [ -z "${CALIBRATION_VALUE}" ]; then
   # Fall back to a default value if per-device data not available
-  LENS_COLOR=$(get_vpd_entry "${VPD_LENS_COLOR_NAME}")
+  LENS_COLOR="$(vpd_get_value "${VPD_LENS_COLOR_NAME}")"
   case "${LENS_COLOR}" in
     "GRAY")
       CALIBRATION_VALUE=${DEFAULT_VALUE_GRAY}
