9701608df05b609df18ad1d3e9dee6618290640a
Daniel Erat <derat@chromium.org>
2017-06-02 11:47:05 -0600

    overlays: Remove overlay-gizmo.
    This board overlay is no longer used.
    
    BUG=chromium:729073
    TEST=none
    CQ-DEPEND=I18602882208a20861c0e8e49d31406390547326c
    
    Change-Id: Iaf47772eb20aec864a3e8891b7980f01d7d6ee18
    Reviewed-on: https://chromium-review.googlesource.com/522864
    Commit-Ready: Dan Erat <derat@chromium.org>
    Tested-by: Dan Erat <derat@chromium.org>
    Reviewed-by: Mike Frysinger <vapier@chromium.org>

--- overlay-gizmo/scripts/board_specific_setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
-
-skip_blacklist_check=1
-skip_test_image_content=1
