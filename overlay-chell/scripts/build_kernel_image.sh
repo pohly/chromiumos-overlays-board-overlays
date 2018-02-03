60602a0b96e96c8602eb342fc021e7b3f7031098
Todd Broch <tbroch@chromium.org>
2018-02-03 10:51:35 -0800

    overlay-chell: Limit intel_idle max_cstate to C7s-SKL on chell.
    BUG=b:65737231, chromium:800881
    TEST=manual,
    1. build & install image on chell
    2. cat /proc/cmdline see 'max_cstate=5'
    3. run powertop and only see C7 as the deepest cstate while previously it was c9
    
    Change-Id: I7bbb65acbb3834dc641614f50772d24275c3cf41
    Reviewed-on: https://chromium-review.googlesource.com/900446
    Commit-Ready: Todd Broch <tbroch@chromium.org>
    Tested-by: Todd Broch <tbroch@chromium.org>
    Reviewed-by: Duncan Laurie <dlaurie@google.com>

---
+++ overlay-chell/scripts/build_kernel_image.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Copyright 2018 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+modify_kernel_command_line() {
+  echo "intel_idle.max_cstate=5" >> "$1"
+}
