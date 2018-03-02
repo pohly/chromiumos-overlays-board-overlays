b805b731028c824843c0d662425bd752a1e2f796
Stephen Barber <smbarber@chromium.org>
2018-03-02 14:26:21 -0800

    termina: add host ip to lxd container config
    BUG=none
    TEST=/dev/.host_ip exists in container
    
    Change-Id: I17ac7ee7c105d963f5fd2eb30f65a13dcf3b2ee7
    Reviewed-on: https://chromium-review.googlesource.com/947034
    Commit-Ready: Stephen Barber <smbarber@chromium.org>
    Tested-by: Stephen Barber <smbarber@chromium.org>
    Reviewed-by: Dylan Reid <dgreid@chromium.org>

--- project-termina/chromeos-base/termina-lxd-scripts/files/lxd_setup.sh
+++ project-termina/chromeos-base/termina-lxd-scripts/files/lxd_setup.sh
@@ -39,6 +39,10 @@ profiles:
       nictype: bridged
       parent: lxdbr0
       type: nic
+    host-ip:
+      path: /dev/.host_ip
+      source: /run/host_ip
+      type: disk
     wayland-sock:
       path: /dev/.wayland-0
       source: /run/wayland-0
@@ -58,6 +62,12 @@ main() {
 
   # Migration: add 0666 mode to default profile for /dev/wl0.
   lxc profile device set default wl0 mode 0666
+
+  # Migration: add host-ip device to default profile.
+  if ! lxc profile device get default host-ip source; then
+    lxc profile device add default host-ip disk \
+        source=/run/host_ip path=/dev/.host_ip
+  fi
 
   # Now that the lxc command has been run, fix up permission for the config.
   chmod 755 "${LXD_CONF}"
