89cbcc0d17527fe2bf1baee5af50aa1dc2a698cf
YH Lin <yueherngl@google.com>
2018-05-22 09:28:22 -0700

    overlay-snappy: format OEM data in ASCII
    Formatting the OEM data in ASCII string instead of raw numeric form.
    
    BUG=b:78240760
    TEST=build and test with chrome://system
    
    Change-Id: Ie0e59c4f1c2ec11f5c78fdd072b9bc5f1104ee41
    Reviewed-on: https://chromium-review.googlesource.com/1069249
    Commit-Ready: YH Lin <yueherngl@chromium.org>
    Tested-by: YH Lin <yueherngl@chromium.org>
    Reviewed-by: Nick Sanders <nsanders@chromium.org>
    Reviewed-by: Todd Broch <tbroch@chromium.org>

--- overlay-snappy/chromeos-base/chromeos-bsp-snappy/files/oemdata.sh
+++ overlay-snappy/chromeos-base/chromeos-bsp-snappy/files/oemdata.sh
@@ -3,10 +3,25 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+# Parameters for Alan/Bigdaddy/Chell/Snappy.
+i2c_bus="4"
+slave_addr="0x0b"
+ct_cmd="0x70"
+data_len="16"
+
 # mosys may use 'i2c-dev', which may not be loaded yet.
 modprobe i2c-dev 2>/dev/null || true
 platform_vendor="$(mosys platform vendor)"
 if [ "${platform_vendor}" = "HP" ]; then
-  # Displays battery CT number.
-  ectool i2cxfer 4 0x0b 16 0x70
+  # Get battery CT number in raw form.
+  # The output format is "Read bytes: <#_bytes> <byte_0> <byte_1> ... <byte_n>".
+  oem_raw_string=$(ectool i2cxfer "${i2c_bus}" "${slave_addr}" "${data_len}" "${ct_cmd}")
+  # Get the number of valid characters -- need to be less than ${data_len}.
+  num_chars=$(/usr/bin/printf "%d" "$(echo "${oem_raw_string}" | cut -d: -f2 | cut -b -4)")
+  # Get the raw CT number.
+  oem_data_string=$(echo "${oem_raw_string}" | cut -d: -f2 | cut -b 5-)
+  # Format the raw CT number in the form of ASCII string.
+  oem_data=$(/usr/bin/printf "%b" "$(echo "${oem_data_string}" | sed -e 's/ *0x/\\\x/g')")
+  # Cut off the trailing/invalid characters and output the OEM data.
+  echo "${oem_data}" | cut -b 1-"${num_chars}"
 fi
