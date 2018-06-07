5771410a432aa31a1bb8e9d6beb9e8528dd77325
YH Lin <yueherngl@google.com>
2018-06-07 11:38:13 -0700

    snappy: update oemdata.sh to deal with some more variations
    BUG=b:109807118
    TEST=build and check with chrome://system
    
    Change-Id: I8ab13116cc49bc60db8f8189ad20e0c1f9a47dbc
    Reviewed-on: https://chromium-review.googlesource.com/1091300
    Commit-Ready: YH Lin <yueherngl@chromium.org>
    Tested-by: YH Lin <yueherngl@chromium.org>
    Reviewed-by: Todd Broch <tbroch@chromium.org>
    Reviewed-by: Mike Frysinger <vapier@chromium.org>

--- overlay-snappy/chromeos-base/chromeos-bsp-snappy/files/oemdata.sh
+++ overlay-snappy/chromeos-base/chromeos-bsp-snappy/files/oemdata.sh
@@ -7,21 +7,17 @@
 i2c_bus="4"
 slave_addr="0x0b"
 ct_cmd="0x70"
-data_len="16"
+data_len="15"
 
 # mosys may use 'i2c-dev', which may not be loaded yet.
 modprobe i2c-dev 2>/dev/null || true
 platform_vendor="$(mosys platform vendor)"
 if [ "${platform_vendor}" = "HP" ]; then
-  # Get battery CT number in raw form.
-  # The output format is "Read bytes: <#_bytes> <byte_0> <byte_1> ... <byte_n>".
-  oem_raw_string=$(ectool i2cxfer "${i2c_bus}" "${slave_addr}" "${data_len}" "${ct_cmd}")
-  # Get the number of valid characters -- need to be less than ${data_len}.
-  num_chars=$(/usr/bin/printf "%d" "$(echo "${oem_raw_string}" | cut -d: -f2 | cut -b -4)")
-  # Get the raw CT number.
-  oem_data_string=$(echo "${oem_raw_string}" | cut -d: -f2 | cut -b 5-)
-  # Format the raw CT number in the form of ASCII string.
-  oem_data=$(/usr/bin/printf "%b" "$(echo "${oem_data_string}" | sed -e 's/ *0x/\\\x/g')")
-  # Cut off the trailing/invalid characters and output the OEM data.
-  echo "${oem_data}" | cut -b 1-"${num_chars}"
+  # Get battery CT number in raw ascii form.
+  # The output format is "\xHH<char_1><char_2>..<char_n>.
+  oem_raw_string=$(ectool --ascii i2cxfer "${i2c_bus}" "${slave_addr}" "${data_len}" "${ct_cmd}")
+  # Get the number of valid characters.
+  num_chars=$(/usr/bin/printf "%d" "0$(echo "${oem_raw_string}" | cut -b 2-4)")
+  # Cut off the trailing/invalid characters (if any) and output the OEM data.
+  echo "${oem_raw_string}" | cut -b 5-$((4+num_chars))
 fi
