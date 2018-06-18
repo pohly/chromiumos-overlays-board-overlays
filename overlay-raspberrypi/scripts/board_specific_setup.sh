101c899bef1ef4492643a8604b1fb74b4150f6db
Gwendal Grignou <gwendal@chromium.org>
2018-06-18 08:51:34 -0700

    Remove duplicate install_hybrid_mbr
    install_hybrid_mbr is already defined in chromeos-common.sh.
    It is run when "hybrid_mbr" is specified in disk_layout.json.
    
    BUG=chromium:853582
    TEST=Trybot all 3 boards, check PMBR and GPT are correct.
    CQ-DEPEND=CL:1104699
    
    Change-Id: I563c9bb3796865e1cce6ffecf5b373f4a147864b
    Signed-off-by: Gwendal Grignou <gwendal@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/1104701
    Tested-by: Gwendal Grignou <gwendal@google.com>
    Reviewed-by: Simon Glass <sjg@chromium.org>

--- overlay-raspberrypi/scripts/board_specific_setup.sh
+++ overlay-raspberrypi/scripts/board_specific_setup.sh
@@ -4,21 +4,6 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-# Creates a hybrid MBR which points the MBR partition 1 to GPT
-# partition 12 (ESP).
-install_hybrid_mbr() {
-  info "Creating hybrid MBR"
-  locate_gpt
-  local start_esp=$(partoffset "$1" 12)
-  local num_esp_sectors=$(partsize "$1" 12)
-  sudo sfdisk -X dos "$1" <<EOF
-unit: sectors
-
-disk1 : start=   ${start_esp}, size=    ${num_esp_sectors}, Id= c, bootable
-disk2 : start=   1, size=    1, Id= ee
-EOF
-}
-
 install_raspberrypi_bootloader() {
   local efi_offset_sectors=$(partoffset "$1" 12)
   local efi_size_sectors=$(partsize "$1" 12)
@@ -45,7 +30,6 @@ install_raspberrypi_bootloader() {
 
 board_setup() {
   install_raspberrypi_bootloader "$1"
-  install_hybrid_mbr "$1"
 }
 
 skip_blacklist_check=1
--- overlay-sama5d3/scripts/board_specific_setup.sh
+++ overlay-sama5d3/scripts/board_specific_setup.sh
@@ -4,21 +4,6 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-# Creates a hybrid MBR which points the MBR partition 1 to GPT
-# partition 12 (ESP).
-install_hybrid_mbr() {
-  info "Creating hybrid MBR"
-  locate_gpt
-  local start_esp=$(partoffset "$1" 12)
-  local num_esp_sectors=$(partsize "$1" 12)
-  sudo sfdisk -X dos "$1" <<EOF
-unit: sectors
-
-disk1 : start=   ${start_esp}, size=    ${num_esp_sectors}, Id= c, bootable
-disk2 : start=   1, size=    1, Id= ee
-EOF
-}
-
 # Downloads and installs the MLO and copies u-boot to the ESP.
 # This is used combined with a hybrid MBR to allow booting
 # a Beaglebone from microSD only.
@@ -42,7 +27,6 @@ install_sama5d3_bootloader() {
 
 board_setup() {
   install_sama5d3_bootloader "$1"
-  install_hybrid_mbr "$1"
 }
 
 skip_blacklist_check=1
