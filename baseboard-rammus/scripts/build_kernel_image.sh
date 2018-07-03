c1d62a4adf33a9e325b54b5217e0ef77df40f80a
Zhuohao Lee <zhuohao@chromium.org>
2018-07-03 15:29:55 +0800

    rammus: add initial rammus overlay
    BUG=b:110006063
    TEST=setup_board, build_packages and build_image pass
    CQ-DEPEND=CL:1126890
    
    Change-Id: I1dd83b1bcf6fd11643638a4b13e2de1538d0d170
    Reviewed-on: https://chromium-review.googlesource.com/1127501
    Commit-Ready: Zhuohao Lee <zhuohao@chromium.org>
    Tested-by: Zhuohao Lee <zhuohao@chromium.org>
    Reviewed-by: YH Lin <yueherngl@chromium.org>

---
+++ baseboard-rammus/scripts/build_kernel_image.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Copyright 2018 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+modify_kernel_command_line() {
+  # Enable S0ix validation check in kernel
+  echo "intel_idle.slp_s0_check=1" >> "$1"
+
+  # Setup S0ix validation initial timeout for slp_s0_check
+  echo "intel_idle.slp_s0_seed=5" >> "$1"
+
+  # Enable S0ix logging using GSMI
+  echo "gsmi.s0ix_logging_enable=1" >> "$1"
+
+  # Might be helpful to preserve ramoops in extreme circumstances
+  echo "ramoops.ecc=1" >> "$1"
+
+  # Backlight driver parameter
+  echo "i915.enable_dpcd_backlight=1" >> "$1"
+
+  # Don't disable the ability to run VMs.
+  echo "disablevmx=off" >> "$1"
+}
