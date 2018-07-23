3a319880986960b287a3e3b938930630e9a5d208
Rajat Jain <rajatja@google.com>
2018-07-23 14:36:37 -0700

    Dragonegg: Set initial kernel parameters
    Setup parameter for ramoops
    
    BUG=b:111760047
    TEST=build
    
    Change-Id: I874fb500a66704adaec10f6188f05ed1a14ea0b5
    Reviewed-on: https://chromium-review.googlesource.com/1147499
    Commit-Ready: Rajat Jain <rajatja@chromium.org>
    Tested-by: Rajat Jain <rajatja@chromium.org>
    Reviewed-by: Rajat Jain <rajatja@chromium.org>

---
+++ baseboard-dragonegg/scripts/build_kernel_image.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Copyright 2018 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+modify_kernel_command_line() {
+  # Might be helpful to preserve ramoops in extreme circumstances
+  echo "ramoops.ecc=1" >> "$1"
+}
