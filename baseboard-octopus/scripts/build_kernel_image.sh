abfe7a5891a496394122448693a3ad8c4753e6bc
Wenkai Du <wenkai.du@intel.com>
2018-07-31 11:06:50 -0700

    octopus: enable S0ix verification check
    Update kernel command line parameters so S0ix verification and
    fail safe are enabled by default.
    
    BUG=b:78841592
    TEST=verify S0iX check happens on yorp board
    
    Change-Id: I9019cc49db70e033146c4cd6ada493bf64c67bd9
    Signed-off-by: Wenkai Du <wenkai.du@intel.com>
    Reviewed-on: https://chromium-review.googlesource.com/1157069
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Reviewed-by: Furquan Shaikh <furquan@chromium.org>
    Reviewed-by: Rajneesh Bhardwaj <rajneesh.bhardwaj@intel.corp-partner.google.com>
    Reviewed-by: Justin TerAvest <teravest@chromium.org>

--- baseboard-octopus/scripts/build_kernel_image.sh
+++ baseboard-octopus/scripts/build_kernel_image.sh
@@ -7,4 +7,7 @@
 modify_kernel_command_line() {
     # Enable S0ix logging using GSMI
     echo "gsmi.s0ix_logging_enable=1" >> "$1"
+
+    # Enable S0ix validation check in kernel
+    echo "intel_idle.slp_s0_check=1" >> "$1"
 }
