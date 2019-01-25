ef38b3d00ef56e41c9e4052bac20224c0b3be40d
Rajat Jain <rajatja@google.com>
2019-01-25 13:43:14 -0800

    hatch: Don't force tpm_tis module
    This basically cherry-pick of
    https://chromium-review.googlesource.com/c/chromiumos/overlays/board-overlays/+/1195763/
    to avoid the same issues on hatch i.e.:
    
    [    1.307344] tpm tpm0: tpm_try_transmit: tpm_send: error -5
    [    2.057962] tpm tpm0: tpm_relinquish_locality: : error -1
    [    2.057993] tpm_tis tpm_tis: 1.2 TPM (device-id 0xFFFF, rev-id 255)
    [    2.058042] tpm tpm0: tpm_try_transmit: tpm_send: error -5
    [    2.808075] tpm tpm0: tpm_relinquish_locality: : error -1
    [    2.808099] tpm tpm0: A TPM error (-5) occurred attempting to
    determine the timeouts
    [    2.808168] tpm_tis: probe of tpm_tis failed with error -5
    
    While here, also add ramoops.ecc=1 since that is a good to have for debugging.
    
    Change-Id: I322949f6e569026800a66b347f192e1b30e60725
    Signed-off-by: Rajat Jain <rajatja@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/1436851
    Commit-Ready: Rajat Jain <rajatja@chromium.org>
    Tested-by: Rajat Jain <rajatja@chromium.org>
    Reviewed-by: Furquan Shaikh <furquan@chromium.org>

---
+++ overlay-hatch/scripts/build_kernel_image.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Copyright 2019 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+modify_kernel_command_line() {
+  # Might be helpful to preserve ramoops in extreme circumstances
+  echo "ramoops.ecc=1" >> "$1"
+
+  # Avoid a cosmetic TPM error (Work around for b/113527055)
+  echo "tpm_tis.force=0" >> "$1"
+}
