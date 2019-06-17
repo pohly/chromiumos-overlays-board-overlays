e1b416b7b4255c0693f07873edf7be9ac3f0936e
Daniel Verkamp <dverkamp@chromium.org>
2019-06-17 09:45:25 -0700

    fizz: re-enable VMX (Crostini)
    Commit 58f92e05f348 ("cfm: fizz|guado Disable USB 3.0 LPM for Huddly Go
    Camera") added build_kernel_image.sh scripts for overlay-fizz and
    overlay-guado, which override the baseboard-fizz build_kernel_image.sh
    script.  The baseboard script sets disablevmx=off, which is necessary to
    turn on hardware virtualization support, so this commit resulted in
    Crostini being unavailable on these boards.
    
    Since there's no support for overlays to inherit baseboard kernel
    command line configuration currently, just include the baseboard
    configuration manually in the overlay scripts to enable VMX again.
    
    BUG=chromium:974338
    TEST=Verify that Linux is available and Terminal can be launched
    
    Change-Id: I029ff7aec80be96699a5d2a04747ab8793eb41e7
    Signed-off-by: Daniel Verkamp <dverkamp@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/1661477
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: Dylan Reid <dgreid@chromium.org>
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    (cherry picked from commit 2174cc2dbeca8c9d2ef31b78efcbbd93066e091c)

--- overlay-fizz/scripts/build_kernel_image.sh
+++ overlay-fizz/scripts/build_kernel_image.sh
@@ -5,6 +5,12 @@
 # found in the LICENSE file.
 
 modify_kernel_command_line() {
+  # disablevmx=off is set by baseboard-fizz, but since build_kernel_image.sh
+  # does not support inheritance, this board-specific file overrides its
+  # definition of modify_kernel_command_line (https://crbug.com/868003),
+  # so include its contents here.
+  echo "disablevmx=off" >> "$1"
+
   # Disable USB 3.0 LPM for Huddly Go
   echo "usbcore.quirks=2bd9:0011:k" >> "$1"
 }
